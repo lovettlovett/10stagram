110b711d2271a259340c277f5d69f1d1bbba9b68
Julia Lovett <julia.m.lovett@gmail.com>
2014-02-18 20:39:21 -0500

    migration change

--- db/migrate/20140219013629_create_handles_table.rb
+++ db/migrate/20140219013629_create_handles_table.rb
@@ -1,6 +1,9 @@
 class CreateHandlesTable < ActiveRecord::Migration
   def change
     create_table :handles_tables do |t|
+    	t.string :handle
+    	t.references :user
+    	t.timestamps
     end
   end
 end
